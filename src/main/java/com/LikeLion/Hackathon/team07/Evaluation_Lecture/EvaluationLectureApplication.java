fbe5ae165fec621ec2e52c18b7b029ee26d4f35f
kjyyjk <rlawnsdud920@gmail.com>
2023-07-05 22:29:57 +0900

    Docs: initial setting

---
+++ src/main/java/com/LikeLion/Hackathon/team07/Evaluation_Lecture/EvaluationLectureApplication.java
@@ -0,0 +1,12 @@
+package com.LikeLion.Hackathon.team07.Evaluation_Lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationLectureApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LikeLion/Hackathon/team07/Evaluation_Lecture/EvaluationLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LikeLion.Hackathon.team07.Evaluation_Lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
