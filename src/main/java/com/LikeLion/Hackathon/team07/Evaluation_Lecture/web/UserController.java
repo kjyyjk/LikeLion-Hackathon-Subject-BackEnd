e12b3440ed272645d269af07d7766333f80b2976
kjyyjk <rlawnsdud920@gmail.com>
2023-07-08 21:18:55 +0900

    Feat: UserJoinRequestDto에 대한 유효성 검사(validation) 기능 추가
    userId, userPassword, userEmail 에 대한 유효성 검사 진행

--- src/main/java/com/LikeLion/Hackathon/team07/Evaluation_Lecture/web/UserController.java
+++ src/main/java/com/LikeLion/Hackathon/team07/Evaluation_Lecture/web/UserController.java
@@ -1,10 +1,17 @@
 package com.LikeLion.Hackathon.team07.Evaluation_Lecture.web;
 
 import com.LikeLion.Hackathon.team07.Evaluation_Lecture.service.UserService;
+import com.LikeLion.Hackathon.team07.Evaluation_Lecture.web.dto.ResultDto;
 import com.LikeLion.Hackathon.team07.Evaluation_Lecture.web.dto.UserJoinRequestDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,9 +23,17 @@ public class UserController {
         return "/userJoinForm";
     }
 
-    @ResponseBody
     @PostMapping("/join")
-    public Long userJoinPro(@RequestBody UserJoinRequestDto requestDto){
-        return userService.join(requestDto);
+    public ResponseEntity userJoinPro(@Valid @RequestBody UserJoinRequestDto requestDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultDto.createFailResult(400,bindingResult));
+        }
+        userService.join(requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(ResultDto.createSuccessResult(200, "회원가입 완료"));
     }
+
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<ResultDto> processValidationError(BindingResult bindingResult){
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultDto.createFailResult(400,bindingResult));
+//    }
 }
