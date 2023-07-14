029412a658a5c92f3f1c60f7454faedfd0ac6d34
kjyyjk <rlawnsdud920@gmail.com>
2023-07-14 12:17:48 +0900

    Feat: 회원탈퇴 기능 추가
    userID와 userPassword를 요청으로 받아 검증,
    회원탈퇴(db삭제) 진행

---
+++ src/main/java/com/LikeLion/Hackathon/team07/Evaluation_Lecture/web/dto/UserJoinRequestDto.java
@@ -0,0 +1,37 @@
+package com.LikeLion.Hackathon.team07.Evaluation_Lecture.web.dto;
+
+import com.LikeLion.Hackathon.team07.Evaluation_Lecture.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@Getter
+
+public class UserJoinRequestDto {
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    private String userID;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    private String userPassword;
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
+    private String userEmail;
+
+    @Builder
+    public UserJoinRequestDto(String userID, String userPassword, String userEmail) {
+        this.userID = userID;
+        this.userPassword = userPassword;
+        this.userEmail = userEmail;
+    }
+
+    public User toEntity(){
+        return User.builder()
+                .userID(userID)
+                .userPassword(userPassword)
+                .userEmail(userEmail)
+                .build();
+    }
+}
